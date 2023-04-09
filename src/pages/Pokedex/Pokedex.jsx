88c1534618dec9035a6a5b4edb1c4aff6fbce3d9
Alexis Almeida <alexis.cesar2017@outlook.com>
2023-04-09 19:33:04 -0300

    Add pagination

--- src/pages/Pokedex/Pokedex.jsx
+++ src/pages/Pokedex/Pokedex.jsx
@@ -9,14 +9,31 @@ import { useEffect, useState } from 'react';
 
 export const Pokedex = () => {
   const [data, setData] = useState([]);
+  const [pokemonsAvailable, setPokemonsAvailable] = useState(0);
+  const [pokemonsPerPage, setPokemonsPerPage] = useState(20);
+  const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
+    defineAmountOfPokemons();
     callPokeAPI();
   }, []);
 
+  useEffect(() => {
+    callPokeAPI();
+  }, [currentPage]);
+
+
+  const defineAmountOfPokemons = async () => {
+    await fetch('https://pokeapi.co/api/v2/pokemon?limit=1')
+    .then(res => res.json())
+    .then(res => setPokemonsAvailable(res.count));
+  };
+
   const callPokeAPI = async () => {
     let endpoints = [];
-    for(let i = 1; i < 21; i++) {
+    let startPoint = 1 + (currentPage * pokemonsPerPage);
+
+    for(let i = startPoint; i < startPoint + pokemonsPerPage; i++) {
       endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}`);
     }
 
@@ -29,11 +46,24 @@ export const Pokedex = () => {
     setData(jsonResponses);
   };
 
+  const handleNextPage = () => {
+    if(currentPage < (Math.floor(pokemonsAvailable / pokemonsPerPage)))
+      setCurrentPage(currentPage + 1);
+  };
+
+  const handlePreviousPage = () => {
+    if(currentPage > 0)
+      setCurrentPage(currentPage - 1);
+  };
 
   return (
+    <>
     <div className={style['title-logo']}>
       <h1>ReactDex!</h1>
       <img src={pokeball} alt="Pokeball logo" />
+      <h2>Pokémons available: {pokemonsAvailable}</h2>
+    </div>
+
 
       <div className={style['cards']}>
         { 
@@ -43,6 +73,14 @@ export const Pokedex = () => {
         }
       </div>
 
-    </div>
+      <div className={style["pagination"]}>
+          <span>Total pages: {Math.floor(pokemonsAvailable / pokemonsPerPage) + 1}</span>
+          <span>Current page: {currentPage + 1}</span>
+        <ul>
+          <button onClick={handlePreviousPage}>Previous Page</button>
+          <button onClick={handleNextPage}>Next Page</button>
+        </ul>
+      </div>
+    </>
   );
 };
